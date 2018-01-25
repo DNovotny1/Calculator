75d4cfbd07b43d9925be28479e6c0a8719b06dac
DNovotny1 <DNovotny1@users.noreply.github.com>
2018-01-25 00:51:02 -0600

    simple calculator
    built for C# advanced week 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secNum = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.multi = new System.Windows.Forms.Button();
+            this.div = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNum
+            // 
+            this.firstNum.Location = new System.Drawing.Point(139, 12);
+            this.firstNum.Name = "firstNum";
+            this.firstNum.Size = new System.Drawing.Size(100, 20);
+            this.firstNum.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Second number";
+            // 
+            // secNum
+            // 
+            this.secNum.Location = new System.Drawing.Point(139, 60);
+            this.secNum.Name = "secNum";
+            this.secNum.Size = new System.Drawing.Size(100, 20);
+            this.secNum.TabIndex = 3;
+            // 
+            // txtOut
+            // 
+            this.txtOut.Enabled = false;
+            this.txtOut.Location = new System.Drawing.Point(95, 217);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(144, 20);
+            this.txtOut.TabIndex = 4;
+            this.txtOut.TabStop = false;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(50, 109);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 5;
+            this.add.Text = "Addition";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(139, 109);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(75, 23);
+            this.sub.TabIndex = 6;
+            this.sub.Text = "Subtraction";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // multi
+            // 
+            this.multi.Location = new System.Drawing.Point(50, 166);
+            this.multi.Name = "multi";
+            this.multi.Size = new System.Drawing.Size(82, 23);
+            this.multi.TabIndex = 7;
+            this.multi.Text = "Multiplication";
+            this.multi.UseVisualStyleBackColor = true;
+            this.multi.Click += new System.EventHandler(this.multi_Click);
+            // 
+            // div
+            // 
+            this.div.Location = new System.Drawing.Point(139, 166);
+            this.div.Name = "div";
+            this.div.Size = new System.Drawing.Size(75, 23);
+            this.div.TabIndex = 8;
+            this.div.Text = "Division";
+            this.div.UseVisualStyleBackColor = true;
+            this.div.Click += new System.EventHandler(this.div_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Output";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.div);
+            this.Controls.Add(this.multi);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.secNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.firstNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox secNum;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button multi;
+        private System.Windows.Forms.Button div;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿//Daniel Novotny
+//Simple C# calculator made for C# advanced
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double a;
+        double b;
+        //clears output, adds and outputs to the two textboxes 
+        private void add_Click(object sender, EventArgs e)
+        {
+            txtOut.Clear(); 
+            a = System.Convert.ToDouble(firstNum.Text);
+            b = System.Convert.ToDouble(secNum.Text);
+            txtOut.AppendText(System.Convert.ToString(a + b));
+        }
+        //clears output, subtracts and outputs to the two textboxes 
+        private void sub_Click(object sender, EventArgs e)
+        {
+            txtOut.Clear();
+            a = System.Convert.ToDouble(firstNum.Text);
+            b = System.Convert.ToDouble(secNum.Text);
+            txtOut.AppendText(System.Convert.ToString(a-b));
+
+        }
+        //clears output, multiplies and outputs to the two textboxes 
+        private void multi_Click(object sender, EventArgs e)
+        {
+            txtOut.Clear();
+            a = System.Convert.ToDouble(firstNum.Text);
+            b = System.Convert.ToDouble(secNum.Text);
+            txtOut.AppendText(System.Convert.ToString(a * b));
+        }
+        //clears output, divides, modulus and outputs to the two textboxes 
+        private void div_Click(object sender, EventArgs e)
+        {
+            txtOut.Clear();
+            a = System.Convert.ToDouble(firstNum.Text);
+            b = System.Convert.ToDouble(secNum.Text);
+            if (b == 0)
+            {
+                txtOut.AppendText("Hey, no dividing by Zero!");
+            }
+            else
+            {
+                txtOut.AppendText(System.Convert.ToString(a / b) + "." + System.Convert.ToString(a % b));
+            }
+        }
+    }
+}
